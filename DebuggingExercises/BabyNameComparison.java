b3ebda9df4c65b2f82bacf9f6708353a632c7c20
genevievepoint <genevieve.point@my.jcu.edu.au>
2016-10-20 09:41:46 +1000

    Debugs and excercises

---
+++ DebuggingExercises/BabyNameComparison.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by Genevieve on 17-Oct-16.
+ */
+public class BabyNameComparison {
+    public static void main(String[] args) {
+        String name1, name2, name3;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a name >> ");
+        name1 = in.nextLine();
+        System.out.print("Enter a second name >> ");
+        name2 = in.nextLine();
+        System.out.print("Enter a third name >> ");
+        name3 = in.nextLine();
+        System.out.println("The combinations are:");
+        System.out.println(name1 + " " + name2);
+        System.out.println(name1 + " " + name3);
+        System.out.println(name2 + " " + name1);
+        System.out.println(name2 + " " + name3);
+        System.out.println(name3 + " " + name1);
+        System.out.println(name3 + " " + name2);
+    }
+}
